59ba02aa03ee5eb560e8bfa143519d2a263a7d2b
keningarcia <kenin.garciacampos@gmail.com>
2026-07-13 23:19:32 -0500

    Creando proyecto

---
+++ src/main/java/com/keningarcia/plantaforma_interno_restaurant/PlantaformaInternoRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.keningarcia.plantaforma_interno_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantaformaInternoRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantaformaInternoRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keningarcia/plantaforma_interno_restaurant/PlantaformaInternoRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keningarcia.plantaforma_interno_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantaformaInternoRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
